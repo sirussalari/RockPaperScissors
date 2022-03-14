aac6621e32121e6fb6bcdc06804d70279f5e05cf
sirussalari <sirus.salari@gmail.com>
2022-03-13 18:56:48 -0700

    Create function to have computer play and function to choose winner
    I created a function that generates a random choice of rock, paper, or scissors. This result is intended to be used in another function that takes the player input as well and then determines the winner of the round.

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
 function computerPlay() {
-    
+    const options = ['rock', 'paper', 'scissors'];
+    let option = Math.floor(Math.random() * 3);
+    let choice = options[option];
+    return choice;
+}
+function playRound(playerSelection, computerSelection) {
+    const lower = playerSelection.toLowerCase();
+    const computerWin = `You lost! ${computerSelection} beats ${lower}`;
+    const playerWin = `You won! ${lower} beats ${computerSelection}`;
+    const tie = `It's a tie! You both chose ${lower}`;
+    if (lower === 'rock') {
+        if (computerSelection === 'rock') {
+            return tie;
+        }
+        else if (computerSelection === 'paper') {
+            return computerWin;
+        }
+        else {
+            return playerWin;
+        }
+    }
+    else if (lower === 'paper') {
+        if (computerSelection === 'rock') {
+            return playerWin;
+        }
+        else if (computerSelection === 'paper') {
+            return tie;
+        }
+        else {
+            return computerWin;
+        }
+    }
+    else {
+        if (computerSelection === 'rock') {
+            return computerWin;
+        }
+        else if (computerSelection === 'paper') {
+            return playerWin;
+        }
+        else {
+            return tie;
+        }
+    }
 }
