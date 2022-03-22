c25b5ebffdcfc9bf60c231bbfb9a5005064283f2
sirussalari <sirus.salari@gmail.com>
2022-03-21 17:21:43 -0700

    Add score keeper and final winner statement
    I added a simple score keeper to the UI that updates the score after every round. After one player reaches a score of 5, I remove the click event from all the buttons and display text that declares thw winner.

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 const buttons = document.querySelectorAll('button');
+const results = document.querySelector('div');
+const playerChoice = document.createElement('p');
+const computerChoice = document.createElement('p');
+const statement = document.createElement('p');
+const playerScore = document.createElement('p');
+const computerScore = document.createElement('p');
+const finalRound = document.createElement('p');
+const computerWin = 'Oh man! You lost this round! :(';
+const playerWin = 'Congrats! You won!';
+const tie = 'This round ended in a tie';
+let playerCount = 0;
+let computerCount = 0;
+results.appendChild(playerChoice);
+results.appendChild(computerChoice);
+results.appendChild(playerScore);
+results.appendChild(computerScore);
+results.appendChild(statement);
+results.appendChild(finalRound);
 function computerPlay() {
     const options = ['rock', 'paper', 'scissors'];
     let option = Math.floor(Math.random() * 3);
@@ -6,48 +24,85 @@ function computerPlay() {
     return choice;
 }
 function playRound(playerSelection, computerSelection) {
+    computerSelection = computerPlay();
     const choice = playerSelection.target.textContent;
     const lower = choice.toLowerCase();
-    const computerWin = 0;
-    const playerWin = 1;
-    const tie = 2;
+    playerChoice.textContent = `Player chooses: ${lower}`
+    computerChoice.textContent = `Computer chooses: ${computerSelection}`
+    playerScore.textContent = `Player Score: ${playerCount}`;
+    computerScore.textContent = `Computer Score: ${computerCount}`;
     if (lower === 'rock') {
         if (computerSelection === 'rock') {
-            return tie;
+            statement.textContent = tie;
         }
         else if (computerSelection === 'paper') {
-            return computerWin;
+            computerCount++;
+            playerScore.textContent = `Player Score: ${playerCount}`;
+            computerScore.textContent = `Computer Score: ${computerCount}`;
+            statement.textContent = computerWin;
         }
         else {
-            return playerWin;
+            playerCount++;
+            playerScore.textContent = `Player Score: ${playerCount}`;
+            computerScore.textContent = `Computer Score: ${computerCount}`;
+            statement.textContent = playerWin;
         }
     }
     else if (lower === 'paper') {
         if (computerSelection === 'rock') {
-            return playerWin;
+            playerCount++;
+            playerScore.textContent = `Player Score: ${playerCount}`;
+            computerScore.textContent = `Computer Score: ${computerCount}`;
+            statement.textContent = playerWin;
         }
         else if (computerSelection === 'paper') {
-            return tie;
+            statement.textContent = tie;
         }
         else {
-            return computerWin;
+            computerCount++;
+            playerScore.textContent = `Player Score: ${playerCount}`;
+            computerScore.textContent = `Computer Score: ${computerCount}`;
+            statement.textContent = computerWin;
         }
     }
     else {
         if (computerSelection === 'rock') {
-            return computerWin;
+            computerCount++;
+            playerScore.textContent = `Player Score: ${playerCount}`;
+            computerScore.textContent = `Computer Score: ${computerCount}`;
+            statement.textContent = computerWin;
         }
         else if (computerSelection === 'paper') {
-            return playerWin;
+            playerCount++
+            playerScore.textContent = `Player Score: ${playerCount}`;
+            computerScore.textContent = `Computer Score: ${computerCount}`;
+            statement.textContent = playerWin;
         }
         else {
-            return tie;
+            statement.textContent = tie;
         }
+    }
+    if (playerCount === 5) {
+        clickEvent(true);
+        results.removeChild(statement);
+        finalRound.textContent = 'Congratulations! You won the game!';
+    }
+    else if (computerCount === 5) {
+        clickEvent(true);
+        results.removeChild(statement);
+        finalRound.textContent = 'You lost :( Better luck next time';
     }
 }
 buttons.forEach(button => {
-    button.addEventListener('click', playRound)
-})
+    button.addEventListener('click', playRound);
+}) 
+function clickEvent(gameOver = false) {
+    if (gameOver) {
+        buttons.forEach(button => {
+            button.removeEventListener('click', playRound);
+        })
+    }
+}
 function game() {
     let playerScore = 0;
     let computerScore = 0;
